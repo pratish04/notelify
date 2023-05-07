ab2e31599e897f63b36ad42e7833e250400f3fd9
Pratish Barthwal <pratishbarthwal@gmail.com>
2023-05-08 01:43:57 +0530

    made some changes

--- server/routes/logoutRoute.js
+++ server/routes/logoutRoute.js
@@ -5,14 +5,16 @@ const authorization = require("../middlewares/authorization");
 
 router.get("/", authorization, (req, res) => {
   try {
-    res
-      .clearCookie("accessToken", {
-        path: "/",
-      })
-      .send({
-        loggedOut: true,
-        message: "SUCCESSFULLY LOGGED OUT!",
-      });
+    res.clearCookie("accessToken", {
+      path: "/",
+    });
+    res.set("Cache-Control", "no-cache, no-store, must-revalidate");
+    res.set("Pragma", "no-cache");
+    res.set("Expires", "0");
+    res.send({
+      loggedOut: true,
+      message: "SUCCESSFULLY LOGGED OUT!",
+    });
   } catch (err) {
     console.error("LOGOUT FAILURE!", err);
     res.send({ message: "LOGOUT FAILURE!" });

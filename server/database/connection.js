a799a08a245c3a89b2abefe5d05437aa37a95eee
Pratish Barthwal <pratishbarthwal@gmail.com>
2023-05-07 16:50:52 +0530

    made some changes

--- server/database/connection.js
+++ server/database/connection.js
@@ -3,10 +3,10 @@ const mysql = require('mysql');
 require("dotenv").config();
 
 const connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
+        host: process.env.HOST,
+        database: process.env.DATABASE,
+        user: process.env.USER,
         password: process.env.DATABASE_PASSWORD,    
-        database: "notes",
         multipleStatements: true,
     });
 

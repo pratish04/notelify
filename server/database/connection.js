f7d15f5fd12851ebd01f8c8f847d0c5c8cf6b4e3
Pratish Barthwal <pratishbarthwal@gmail.com>
2023-05-09 20:00:58 +0530

    made some changes

--- server/database/connection.js
+++ server/database/connection.js
@@ -2,37 +2,39 @@ const express = require("express");
 const mysql = require("mysql");
 require("dotenv").config();
 
-const connection = mysql.createConnection({
+const dbConfig = {
   host: process.env.HOST,
   database: process.env.DATABASE,
   user: process.env.USER,
   password: process.env.DATABASE_PASSWORD,
   multipleStatements: true,
-});
+};
+
+let connection = mysql.createConnection(dbConfig);
+
+function handleDisconnect(){
+  connection=mysql.createConnection(dbConfig);
+}
 
 connection.connect((err) => {
   if (err) {
     console.error("ERROR CONNECTING TO THE DATABASE!", err);
+    setTimeout(handleDisconnect, 2000);
     return;
   }
   console.log("CONNECTED TO THE DATABASE!");
 });
+
 connection.on("error", (err) => {
   if (err.code === "PROTOCOL_CONNECTION_LOST") {
     console.error("DATABASE CONNECTION CLOSED!");
+    handleDisconnect();
   } else if (err.code === "ETIMEDOUT") {
     console.error("DATABASE CONNECTION TIMEOUT!");
+    handleDisconnect();
   } else {
-    console.error("Database error occurred: ", err);
+    console.error("Database error occurred!", err);
   }
-
-  connection.connect((err) => {
-    if (err) {
-      console.error("ERROR CONNECTING TO THE DATABASE!", err);
-    } else {
-      console.log("RECONNECTED TO THE DATABASE!");
-    }
-  });
 });
 
 module.exports = connection;

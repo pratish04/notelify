2d6ddb076c6f71ba823ad70d4dfdb5bcdb73c501
Pratish Barthwal <pratishbarthwal@gmail.com>
2023-10-05 18:23:47 +0530

    made some changes

--- server/database/redisClient.js
+++ server/database/redisClient.js
@@ -3,7 +3,13 @@ const redis = require("redis");
 
 require("dotenv").config();
 
-const redisClient = redis.createClient();
+const redisClient = redis.createClient({
+  password: process.env.REDIS_PASSWORD,
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+  },
+});
 
 redisClient.connect();
 
